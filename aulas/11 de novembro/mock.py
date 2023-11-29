0a6b750d89c05f9e6074ca210a900c08f2894891
Yasmim Brennda Azevedo Souza <s.yasmim@academico.ifrn.edu.br>
2023-11-29 11:55:48 -0300

    11 de novembro

---
+++ aulas/11 de novembro/mock.py
@@ -0,0 +1,25 @@
+# Quando a API não está disponível mas se quer testar mesmo assim, chamar um método criado para se passar pela API;
+# retorna uma resposta esperada, como se fosse a resposta que seria dada pela API.
+
+from random import randint
+def velocidade():
+
+    return randint(40,120)
+    #retorna números aleatórios entre 40 e 120;
+
+def alerta():
+
+    v = velocidade()
+    if v < 60 or v > 100:
+        
+        return True
+        # v fora do intervalo de velocidade aceita
+    
+    return False
+
+from unittest.mock import Mock
+mock_velocidade = Mock()
+# +++++
+
+# Pra não precisar depender da velocidade gerar um número aleatório entre 60 e 100, cria um mock que dá sempre um valor entre esses números.
+
